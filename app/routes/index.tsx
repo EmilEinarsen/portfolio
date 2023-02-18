ac3025a636f311950e1a43c5e7d1c9910e3b5fa5
EmilEinarsen <emil@hagatun.se>
2023-02-18 17:00:16 -0500

    Remix + Tailwind starter

---
+++ app/routes/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+  return (
+    <div className="h-full w-full grid place-content-center">
+      <h1 className="text-9xl font-extrabold text-red-600 uppercase">Gottem!</h1>
+    </div>
+  );
+}
