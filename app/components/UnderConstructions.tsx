67064d2db29f7d4a4265d93230104d23548bc6b8
EmilEinarsen <emil@hagatun.se>
2023-02-22 01:40:51 -0500

    fix: :sparkles: adjust underconstruction banner for mobile
    adjust placeholder theme to better suit mobile and be slightly less obnoxious

--- app/components/UnderConstructions.tsx
+++ app/components/UnderConstructions.tsx
@@ -3,14 +3,14 @@
 import { useTheme } from '~/utils/theme-provider'
 import { Link } from './Link'
 
-const faceSize = 300
+const faceSize = 250
 const atlasToFaceRatio = 13/16
 
 export const UnderConstructions = () => {
   const [ theme ] = useTheme()
   return (
     <div className="h-screen w-full flex flex-col gap-12 justify-center">
-      <h1 className="text-4xl sm:text-5xl md:text-7xl font-extrabold text-orange-500 uppercase flex flex-col xl:flex-row flex-wrap text-center justify-center gap-2 md:gap-6 h-fit">
+      <h1 className="text-4xl sm:text-5xl md:text-6xl font-extrabold text-orange-500 uppercase flex flex-col xl:flex-row flex-wrap text-center justify-center gap-2 md:gap-6 h-fit">
         <span className="not-sr-only">🚧👷</span>
         Construction ahead
         <span className="not-sr-only">🔨🚧</span>
@@ -20,7 +20,7 @@ export const UnderConstructions = () => {
           <img src='/profile.webp' alt="Emil Einarsen" className="aspect-square rotate-12 pointer-events-none" width={faceSize} />
           <span className='sr-only'>Emil Einarsens Github Profile</span>
         </Link>
-        <img src={`/atlas${theme === 'dark' ? '-inverted' : ''}.webp`} alt="Greek God Atlas" className="mx-auto translate-x-6 -translate-y-[31%] pointer-events-none" width={faceSize * atlasToFaceRatio} />
+        <img src={`/atlas${theme === 'dark' ? '-inverted' : ''}.webp`} alt="Greek God Atlas" className="mx-auto translate-x-5 -translate-y-[31%] pointer-events-none" width={faceSize * atlasToFaceRatio} />
       </div>
     </div>
   )
